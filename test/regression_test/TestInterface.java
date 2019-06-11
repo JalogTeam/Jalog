11afb8c0d2593b953507fdb6cfa04a3f35cdf27d
Ari Okkonen <ari.okkonen@gmail.com>
2019-06-12 00:53:57 +0300

    Java interface tested for integer, symbol, real, character, and string arguments.

--- test/regression_test/TestInterface.java
+++ test/regression_test/TestInterface.java
@@ -14,8 +14,17 @@ public class TestInterface
     System.out.println(id_string);
     myJalog.consult_file("test_java_if.pro");
     long int_value;
-    Jalog.Term in_int, out_int, out_comment;
+    String string_value;
+    double real_value;
+    char char_value;
+    Jalog.Term in_int, out_int, 
+        in_symbol, out_symbol, 
+        in_real, out_real,
+        in_char, out_char,
+        in_string, out_string,
+        out_comment;
     
+    // integer
 
     in_int = Jalog.integer(7);
     out_int = Jalog.open();
@@ -35,10 +44,131 @@ public class TestInterface
         }
 
         print_comment(out_comment);
+      } else {
+         System.out.println("test_int_fail");
+       
       }
     } catch (Jalog.Exit e) {
       System.out.println("Exit " + e);
     }
+    System.out.println();
+
+    // symbol
+
+    in_symbol = Jalog.symbol("test");
+    out_symbol = Jalog.open();
+    out_comment = Jalog.open();
+    
+    try {
+      if (myJalog.call("test_symbol", in_symbol, out_symbol, out_comment))
+      {
+        System.out.println("test_symbol_success");
+        type = out_symbol.getType();
+        System.out.println("out_symbol of type " + type);
+        if (type == Jalog.SYMBOL) {
+          string_value = out_symbol.getSymbolValue();
+          System.out.println("out_symbol = " + string_value);
+        } else {
+          System.out.println("out_symbol bad");
+        }
+
+        print_comment(out_comment);
+      } else {
+         System.out.println("test_symbol_fail");
+       
+      }
+    } catch (Jalog.Exit e) {
+      System.out.println("Exit " + e);
+    }
+    System.out.println();
+
+    // real
+
+    in_real = Jalog.real(3.142);
+    out_real = Jalog.open();
+    out_comment = Jalog.open();
+    
+    try {
+      if (myJalog.call("test_real", in_real, out_real, out_comment))
+      {
+        System.out.println("test_real_success");
+        type = out_real.getType();
+        System.out.println("out_real of type " + type);
+        if (type == Jalog.REAL) {
+          real_value = out_real.getRealValue();
+          System.out.println("out_real = " + real_value);
+        } else {
+          System.out.println("out_real bad");
+        }
+
+        print_comment(out_comment);
+      } else {
+         System.out.println("test_real_fail");
+       
+      }
+    } catch (Jalog.Exit e) {
+      System.out.println("Exit " + e);
+    }
+    System.out.println();
+
+    // character
+
+    in_char = Jalog.character('y');
+    out_char = Jalog.open();
+    out_comment = Jalog.open();
+    
+    try {
+      if (myJalog.call("test_char", in_char, out_char, out_comment))
+      {
+        System.out.println("test_char_success");
+        type = out_char.getType();
+        System.out.println("out_char of type " + type);
+        if (type == Jalog.CHARACTER) {
+          char_value = out_char.getCharacterValue();
+          System.out.println("out_char = '" + char_value + "'");
+        } else {
+          System.out.println("out_char bad");
+        }
+
+        print_comment(out_comment);
+      } else {
+         System.out.println("test_char_fail");
+       
+      }
+    } catch (Jalog.Exit e) {
+      System.out.println("Exit " + e);
+    }
+    System.out.println();
+
+    // string
+
+    in_string = Jalog.string("template");
+    out_string = Jalog.open();
+    out_comment = Jalog.open();
+    
+    try {
+      if (myJalog.call("test_string", in_string, out_string, out_comment))
+      {
+        System.out.println("test_string_success");
+        type = out_string.getType();
+        System.out.println("out_string of type " + type);
+        if (type == Jalog.STRING) {
+          string_value = out_string.getStringValue();
+          System.out.println("out_string = \"" + string_value + "\"");
+        } else {
+          System.out.println("out_string bad");
+        }
+
+        print_comment(out_comment);
+      } else {
+         System.out.println("test_string_fail");
+       
+      }
+    } catch (Jalog.Exit e) {
+      System.out.println("Exit " + e);
+    }
+    System.out.println();
+
   }
   
   public static void print_comment(Jalog.Term out_comment) {
--- test/regression_test/test_java_if.pro
+++ test/regression_test/test_java_if.pro
@@ -1,6 +1,31 @@
+% integer
+test_int(5, 0, "NOK?"). % open?
 test_int(7, 9, "OK").
 test_int(X, 0, "NOK").
 
+% symbol
+test_symbol(trap, is_open, "NOK?").
+test_symbol(test, good, "OK").
+test_symbol(X, ugly, "NOK").
 
+% real
+test_real(2.718, 0.0, "NOK?"). % open?
+test_real(3.142, 1.414, "OK").
+test_real(X, 0.0, "NOK").
+
+% character
+test_char('x', '@', "NOK?").
+test_char('y', 'z', "OK").
+test_char(X, '#', "NOK").
+
+
+% string
+test_string("whatever", "is open", "NOK?").
+test_string("template", "match", "OK").
+test_string(X, "fallback", "NOK").
+
+% list
+
+% compound
   
   
