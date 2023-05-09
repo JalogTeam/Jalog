2adae997829df25962a74f5514a49c8fe5726cc2
Ari Okkonen <ari.okkonen@gmail.com>
2023-05-09 17:43:38 +0300

    BuiltIns.java and Version.java included in jar file

--- build_jar.bat
+++ build_jar.bat
@@ -7,5 +7,11 @@ javac -d classes -cp classes -sourcepath src src/io/github/JalogTeam/jalog/Jalog
 
 xcopy src\META-INF classes\META-INF /S /I /Y
 xcopy resources classes  /S /I /Y
+
+mkdir classes\src\io\github\JalogTeam\jalog
+
+copy src\io\github\JalogTeam\jalog\Version.java classes\src\io\github\JalogTeam\jalog\Version.java
+copy src\io\github\JalogTeam\jalog\BuiltIns.java classes\src\io\github\JalogTeam\jalog\BuiltIns.java 
+
 jar -cfM jalog.jar -C classes .
 
