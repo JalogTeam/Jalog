30825ac2dfb44cb65e883fb937b470831fa3ef38
Ari Okkonen <ari.okkonen@gmail.com>
2023-10-08 23:36:15 +0300

    starting r1.5, new FileManager.java

--- src/io/github/JalogTeam/jalog/Version.java
+++ src/io/github/JalogTeam/jalog/Version.java
@@ -3,5 +3,5 @@ package io.github.JalogTeam.jalog;
 
 public class Version {
   public static final String id_string =
-      "Jalog 1.4.0 by Ari Okkonen & Mikko Levanto 2023-09-13";
+      "Jalog 1.5.0 by Ari Okkonen & Mikko Levanto 2023-10-04";
 }
