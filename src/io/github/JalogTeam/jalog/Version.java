b0377a6e79943e2682d7cb3486fb3b80223feb6b
Ari Okkonen <ari.okkonen@gmail.com>
2024-03-13 17:14:31 +0200

    Version 1.4.1 ready to release

--- src/io/github/JalogTeam/jalog/Version.java
+++ src/io/github/JalogTeam/jalog/Version.java
@@ -3,5 +3,5 @@ package io.github.JalogTeam.jalog;
 
 public class Version {
   public static final String id_string =
-      "Jalog 1.4.1 by Ari Okkonen & Mikko Levanto 2024-03-07";
+      "Jalog 1.4.1 by Ari Okkonen & Mikko Levanto 2024-03-13";
 }
